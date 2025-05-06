6a9c1fb4908d6a1168e0ec0a947937857859b2e7
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2025-05-06 13:34:57 +0200

    Tog bort run-firefox-hjälpfilen - på riktigt

--- run-firefox.js
+++
@@ -1,9 +0,0 @@
-const { Builder } = require('selenium-webdriver');
-require('geckodriver');
-
-(async () => {
-  let driver = await new Builder().forBrowser('firefox').build();
-  await driver.get('https://example.com');
-  await driver.sleep(3000); 
-  await driver.quit();
-})();
